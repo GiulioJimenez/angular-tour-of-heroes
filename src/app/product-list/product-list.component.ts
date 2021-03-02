e59a712dce934440bd803e950204552e8abeb522
GiulioJimenez <giulioabel10@gmail.com>
2021-03-02 10:21:32 +0100

    Prima della pausa

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,7 +10,16 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
+  constructor() { }
+
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+  ngOnInit(): void {
   }
 }
